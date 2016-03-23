97b12da752bc577acd4ba90a46238e8ac2a599e4
smileShirely <meiyicao@163.com>
2016-03-23 21:43:45 +0800

    改善图表

--- js/form.js
+++ js/form.js
@@ -1,18 +1,17 @@
 /**
  * Created by cmy on 2016/3/19.
  */
+
 var tbody = document.getElementById('tBody');
-var tbodyRows = tbody.rows.length;
-console.log(tbodyRows);
+/*var tbodyRows = tbody.rows.length;*/
 var intRowIndex = 0;
+//在表格底部再插入一行
 function insertRow(tbIndex) {
 	var objRow = document.getElementById('infoData').insertRow(tbIndex);
 	table.appendChild(objRow);
 	var objCel1 = objRow.insertCell(0);
 	objCel1.innerHTML = '<input type="checkbox">';
-	objCel1.parentNode.addEventListener("click", getChecked(),false);
-	/*var tdFront = $('tBody').getElementsByTagName("tr").childNodes[1];
-	 tdFront.addEventListener("click", getChecked(),false);*/
+	objCel1.parentNode.addEventListener("click", getChecked(), false);
 	var objCel2 = objRow.insertCell(1);
 	objCel2.innerText = document.addForm.myCell1.value;
 	var objCel3 = objRow.insertCell(2);
@@ -23,83 +22,135 @@ function insertRow(tbIndex) {
 	objCel5.innerText = document.addForm.myCell4.value;
 	var objCel6 = objRow.insertCell(5);
 	objCel6.innerText = document.addForm.myCell5.value;
-
 	objRow.addEventListener("click", getIndex, false);
 
 	console.log(objRow);
-	/*var row = document.createElement("tr");
-	tbody.appendChild(row);
-	var cell_1 = document.createElement("td");
-	cell_1.appendChild(document.createTextNode("document.addForm.myCell1.value"));*/
 }
 
-/*
-/!* 获取信息*!/
-function trContent(table) {;
-	var td = event.srcElement; // 通过event.srcElement 获取激活事件的对象 td
-	var rowNum = td.parentElement.rowIndex;//tr的行数
-	var tableRow = document.getElementById('table').rows[rowNum];
-	for ( var n=1; n<= 5; n++) {
-		tableRow.cells[n].innerText = document.infoForm.myCell+n.value;
-	}
-}
-*/
 
-function deletRow(tbIndex) {
-	var Row = tbIndex.parentNode;
-	while (Row.tagName.toLowerCase()!="tr")
-	{
-		Row = Row.parentNode;
-	}
-	Row.parentNode.removeChild(Row);
-}
 
 function getIndex(tbIndex) {
 	intRowIndex = event.srcElement.parentElement.rowIndex;
 	console.log(insertRow);
 }
-/*进行队列操作*/
 
-/*var input = $('infoData').getElementsByTagName("input");*/
-
-/*function inputCheckbox() {
-	var td = event.srcElement; // 通过event.srcElement 获取激活事件的对象 td
-	var rowNum = td.parentElement.rowIndex;
-	return rowNum;
-	alert("rowNum"c);
-}
-$(s).addEventListener(click,inputCheckbox,false);*/
-
-/*没有用到的*/
-
-
-//弹出确认是否删除的对话框
-/*function showDiv() {
- document.getElementById('popDiv').style.display="block";
- document.getElementById('affirmClose-bg').style.display="block";
- }*/
-function closeDiv() {
-	document.getElementById('popDiv').style.display="none";
-	document.getElementById('affirmClose-bg').style.display="none";
-}
-//获取table中的行号和内容
-function doclick() {
-	var td = event.srcElement.parentElement; // 通过event.srcElement 获取激活事件的对象 td
-/*	var rowNum = td.parentElement.rowIndex;
-	return rowNum;*/
-	/*alert("行号：" + (td.parentElement.rowIndex) + "，内容：" + td.innerText);*/
-}
 var rowIndex;
 var checkboxs = document.getElementsByName("checks");
-var tbody = document.getElementsByTagName("tbody");
+/*var tbody = document.getElementsByTagName("tbody");*/
 var table = document.getElementById("infoData");
+//获取行号
 function getChecked(){
+	var myChart = echarts.init(document.getElementById('main'));
+	option2 = {
+		title : {
+			text : '数据统计图'
+		},
+		tooltip : {
+			trigger: 'item',
+			formatter : function (params) {
+				var date = new Date(params.value[0]);
+				data = date.getFullYear() + '-'
+				  + (date.getMonth() + 1) + '-'
+				  + date.getDate() + ' '
+				  + date.getHours() + ':'
+				  + date.getMinutes();
+				return data + '<br/>'
+				  + params.value[1] + ', '
+				  + params.value[2];
+			}
+		},
+		toolbox: {
+			show : true,
+			feature : {
+				mark : {show: true},
+				dataView : {show: true, readOnly: false},
+				restore : {show: true},
+				saveAsImage : {show: true}
+			}
+		},
+		dataZoom: {
+			show: true,
+			start : 70
+		},
+		legend : {
+			data : []
+		},
+		grid: {
+			y2: 80
+		},
+		xAxis : [
+			{
+				type : 'time',
+				splitNumber:10
+			}
+		],
+		yAxis : [
+			{
+				type : 'value'
+			}
+		],
+		series : [
+			{
+				name: 1,
+				type: 'line',
+				showAllSymbol: true,
+				symbolSize: function (value){
+					return Math.round(value[2]/10) + 2;
+				},
+				data: (function () {
+					var d = [];
+					var len = 0;
+					var now = new Date();
+					var value;
+					while (len++ < 200) {
+						d.push([
+							new Date(2016, 1, 1, 0, len * 1000),
+							(Math.random()*200).toFixed(2) - 0,
+							(Math.random()*100).toFixed(2) - 0
+						]);
+					}
+					return d;
+				})()
+			}
+		]
+	};
+
+	myChart.setOption(option2);
 	for (var i= 0, len= checkboxs.length;i<len; i++) {
 			if (checkboxs[i].checked == true) {
 				var sTr = checkboxs[i].parentNode.parentNode;
-				rowIndex = i+2;
-				console.log("行号：" + rowIndex + "内容:" + table.rows[rowIndex].cells[2].innerText);
+				rowIndex = i+1;
+				option1.legend.data[i+1]= i.toString();
+				option1.series[1+i]={
+					name: i.toString(),
+					type: 'line',
+					showAllSymbol: true,
+					symbolSize: function (value){
+						return Math.round(value[2]/10) + 2;
+					},
+					data: (function () {
+						var d = [];
+						var len = 0;
+						var now = new Date();
+						var value;
+						while (len++ < 200) {
+							d.push([
+								new Date(2016, 1, 1, 0, len * 1000),
+								(Math.random()*200).toFixed(2) - 0,
+								(Math.random()*100).toFixed(2) - 0
+							]);
+						}
+						return d;
+					})()
+				};
+				myChart.setOption(option1);
+				/*console.log("行号：" + rowIndex + "内容:" + table.rows[rowIndex].cells[2].innerText);*/
+
 			}
 		}
-	console.log(rowIndex);
+/*	console.log(rowIndex);*/
 }
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -87,118 +87,21 @@ $(function() {
 	/*图表*/
 	var myChart = echarts.init(document.getElementById('main'));
 	option1 = {
-		title : {//标题组件，包含主标题和副标题
-			text : '统计图',
-			show : true
-		},
-		tooltip : {//提示框组件
-			trigger: 'item',
-			formatter : function (params) {
-				var date = new Date(params.value[0]);
-				data = date.getFullYear() + '-'
-					+ (date.getMonth() + 1) + '-'
-					+ date.getDate() + ' '
-					+ date.getHours() + ':'
-					+ date.getMinutes();
-				return data + '<br/>'
-					+ params.value[1] + ', '
-					+ params.value[2];
-			}
-		},
-		toolbox: {//工具栏。内置有导出图片，数据视图，动态类型切换，数据区域缩放，重置五个工具。
-			show : true,
-			feature : {
-				mark : {show: true},
-				dataView : {show: true, readOnly: false},
-				restore : {show: true},
-				saveAsImage : {show: true}
-			}
-		},
-		dataZoom: {//用于对数据进行区域缩放，从而能自由关注细节的数据信息，或者概览数据整体
-			show: true,
-			start : 70
-		},
-		legend : {//图例组件展现了不同系列的标记(symbol)，颜色和名字。可以通过点击图例控制哪些系列不显示
-			data : ['series1'],
-			z : 1
-		},
-		grid: {//直角坐标系内绘图网格，单个 grid 内最多可以放置上下两个 X 轴，左右两个 Y 轴。可以在网格上绘制折线图，柱状图，散点图（气泡图）
-			y2: 80
-		},
-		xAxis : [//直角坐标系grid中的x轴，单个 grid 组件最多只能放上下两个 x 轴
-			{
-				type : 'time',
-				splitNumber:10
-			}
-		],
-		yAxis : [//直角坐标系 grid 中的 y 轴，单个 grid 组件最多只能放左右两个 y 轴
-			{
-				type : 'value'
-			}
-		],
-		series : [//系列列表。每个系列通过 type 决定自己的图表类型
-			{
-				name: '1',
-				type: 'line',
-				showAllSymbol: true,
-				symbolSize: function (value){
-					return Math.round(value[2]/10) + 2;
-				},
-				data: (function () {
-					var d = [];
-					var len = 0;
-					var now = new Date();
-					var value;
-					while (len++ < 200) {
-						d.push([
-							new Date(2014, 9, 1, 0, len * 3800),
-							(Math.random()*30).toFixed(2) - 0,
-							(Math.random()*100).toFixed(2) - 0
-						]);
-					}
-					return d;
-				})()
-			},
-			{
-				name: '2',
-				type: 'line',
-				showAllSymbol: true,
-				symbolSize: function (value){
-					return Math.round(value[2]/10) + 2;
-				},
-				data: (function () {
-					var d = [];
-					var len = 0;
-					var now = new Date();
-					var value;
-					while (len++ < 200) {
-						d.push([
-							new Date(2014, 9, 1, 0, len * 3800),
-							(Math.random()*90).toFixed(2) - 0,
-							(Math.random()*100).toFixed(2) - 0
-						]);
-					}
-					return d;
-				})()
-			}
-		]
-	};
-	/*option2 = {
 		title : {
-			text : '数据图',
+			text : '数据统计图'
 		},
 		tooltip : {
 			trigger: 'item',
 			formatter : function (params) {
 				var date = new Date(params.value[0]);
 				data = date.getFullYear() + '-'
-					+ (date.getMonth() + 1) + '-'
-					+ date.getDate() + ' '
-					+ date.getHours() + ':'
-					+ date.getMinutes();
+				  + (date.getMonth() + 1) + '-'
+				  + date.getDate() + ' '
+				  + date.getHours() + ':'
+				  + date.getMinutes();
 				return data + '<br/>'
-					+ params.value[1] + ', '
-					+ params.value[2];
+				  + params.value[1] + ', '
+				  + params.value[2];
 			}
 		},
 		toolbox: {
@@ -215,8 +118,7 @@ $(function() {
 			start : 70
 		},
 		legend : {
-			data : ['series2'],
-			z : 2
+			data : ['1']
 		},
 		grid: {
 			y2: 80
@@ -234,31 +136,51 @@ $(function() {
 		],
 		series : [
 			{
-				name: '1',
 				type: 'line',
 				showAllSymbol: true,
 				symbolSize: function (value){
 					return Math.round(value[2]/10) + 2;
-				},
-				data: (function () {
-					var d = [];
-					var len = 0;
-					var now = new Date();
-					var value;
-					while (len++ < 200) {
-						d.push([
-							new Date(2014, 9, 1, 0, len * 3800),
-							(Math.random()*40).toFixed(2) - 0,
-							(Math.random()*100).toFixed(2) - 0
-						]);
-					}
-					return d;
-				})()
+				}
+
 			}
 		]
-	};*/
+	};
 
 	myChart.setOption(option1);
+
+	/*点击增加*/
+	/*for (i =0, len=checkboxs.length; i<len;i++) {
+		console.log(i);
+			checkboxs[i].addEventListener("click",function() {
+				console.log(i);
+					option1.legend.data[i+1]= i.toString();
+					option1.series[1+i]={
+						name: i.toString(),
+						type: 'line',
+						showAllSymbol: true,
+						symbolSize: function (value){
+							return Math.round(value[2]/10) + 2;
+						},
+						data: (function () {
+							var d = [];
+							var len = 0;
+							var now = new Date();
+							var value;
+							while (len++ < 200) {
+								d.push([
+									new Date(2016, 1, 1, 0, len * 1000),
+									(Math.random()*200).toFixed(2) - 0,
+									(Math.random()*100).toFixed(2) - 0
+								]);
+							}
+							return d;
+						})()
+					};
+					myChart.setOption(option1);
+			},false);
+	}*/
+
+
 
 	/*搜索框中的内容*/
 	$(document).ready(function() {
@@ -287,7 +209,4 @@ $(function() {
 			else {$('.no-result').hide();}
 		});
 	});
-
-	/*添加数据*/
-
 });
