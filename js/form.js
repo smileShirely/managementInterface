3058254587f13a7620f36c71634bc958b169b9fa
smileShirely <meiyicao@163.com>
2016-03-23 15:21:49 +0800

    完成了队列的操作

--- js/form.js
+++ js/form.js
@@ -1,11 +1,18 @@
 /**
  * Created by cmy on 2016/3/19.
  */
+var tbody = document.getElementById('tBody');
+var tbodyRows = tbody.rows.length;
+console.log(tbodyRows);
 var intRowIndex = 0;
 function insertRow(tbIndex) {
 	var objRow = document.getElementById('infoData').insertRow(tbIndex);
+	table.appendChild(objRow);
 	var objCel1 = objRow.insertCell(0);
-	objCel1.innerHTML = '<a href="#"><input type="checkbox"></a>';
+	objCel1.innerHTML = '<input type="checkbox">';
+	objCel1.parentNode.addEventListener("click", getChecked(),false);
+	/*var tdFront = $('tBody').getElementsByTagName("tr").childNodes[1];
+	 tdFront.addEventListener("click", getChecked(),false);*/
 	var objCel2 = objRow.insertCell(1);
 	objCel2.innerText = document.addForm.myCell1.value;
 	var objCel3 = objRow.insertCell(2);
@@ -15,10 +22,19 @@ function insertRow(tbIndex) {
 	var objCel5 = objRow.insertCell(4);
 	objCel5.innerText = document.addForm.myCell4.value;
 	var objCel6 = objRow.insertCell(5);
-	objCel6.innerText = document.addForm.myCell15.value;
-	objRow.attachEvent("onclick", getIndex);
+	objCel6.innerText = document.addForm.myCell5.value;
+
+	objRow.addEventListener("click", getIndex, false);
+
+	console.log(objRow);
+	/*var row = document.createElement("tr");
+	tbody.appendChild(row);
+	var cell_1 = document.createElement("td");
+	cell_1.appendChild(document.createTextNode("document.addForm.myCell1.value"));*/
 }
-/* 获取信息*/
+
+/*
+/!* 获取信息*!/
 function trContent(table) {;
 	var td = event.srcElement; // 通过event.srcElement 获取激活事件的对象 td
 	var rowNum = td.parentElement.rowIndex;//tr的行数
@@ -27,6 +43,7 @@ function trContent(table) {;
 		tableRow.cells[n].innerText = document.infoForm.myCell+n.value;
 	}
 }
+*/
 
 function deletRow(tbIndex) {
 	var Row = tbIndex.parentNode;
@@ -39,9 +56,8 @@ function deletRow(tbIndex) {
 
 function getIndex(tbIndex) {
 	intRowIndex = event.srcElement.parentElement.rowIndex;
-	pos.innerText = intRowIndex;
+	console.log(insertRow);
 }
-
 /*进行队列操作*/
 
 /*var input = $('infoData').getElementsByTagName("input");*/
@@ -69,7 +85,21 @@ function closeDiv() {
 //获取table中的行号和内容
 function doclick() {
 	var td = event.srcElement.parentElement; // 通过event.srcElement 获取激活事件的对象 td
-	var rowNum = td.parentElement.rowIndex;
-	return rowNum;
+/*	var rowNum = td.parentElement.rowIndex;
+	return rowNum;*/
 	/*alert("行号：" + (td.parentElement.rowIndex) + "，内容：" + td.innerText);*/
+}
+var rowIndex;
+var checkboxs = document.getElementsByName("checks");
+var tbody = document.getElementsByTagName("tbody");
+var table = document.getElementById("infoData");
+function getChecked(){
+	for (var i= 0, len= checkboxs.length;i<len; i++) {
+			if (checkboxs[i].checked == true) {
+				var sTr = checkboxs[i].parentNode.parentNode;
+				rowIndex = i+2;
+				console.log("行号：" + rowIndex + "内容:" + table.rows[rowIndex].cells[2].innerText);
+			}
+		}
+	console.log(rowIndex);
 }
--- js/main.js
+++ js/main.js
@@ -5,9 +5,16 @@ $(function() {
 	var opera = $('#operation');
 	var menu = $('#info ul');
 	$(opera).click(function() {
-		$(menu).toggle();
+		if (rowIndex == null) {
+			$('#error').css({'display':'block'});
+		} else {
+			$(menu).toggle();
+		}
 	});
-	//
+	//关闭错误提示框
+	$('#answer').click(function() {
+		$('#error').css({'display':'none'});
+	});
 	//弹出新建队列的表单
 	$('#new').click(function() {
 		$('#addForm').css({'display':'block'});
@@ -17,7 +24,7 @@ $(function() {
 		 insertRow(infoData.rows.length);
 	});
 	//取消
-	$('#cancel').click(function() {
+	$('#cancel,#add').click(function() {
 		$('#addForm').css({'display':'none'});
 	});
 	///* 拖动div */
@@ -28,37 +35,63 @@ $(function() {
 /* 弹出查看信息的表单*/
 
 	$('#check').click(function() {
+		$(menu).toggle();
 		$('#infoForm').css({'display':'block'});
-	});
+		var infoForm = document.getElementById('infoForm');
+		for (var i = 0; i <= 4; i++) {
+			infoForm.elements[i].value = table.rows[rowIndex].cells[i+1].innerText;
+		}
 
+	});
 	$('#close').click(function() {
 		$('#infoForm').css({'display':'none'});
 	});
 
 /* 弹出删除信息 */
 	$('#delete').click(function() {
+		$(menu).toggle();
 		$('#delForm').css({'display':'block'});
+		var delForm = document.getElementById('delForm');
+		for (var i = 0; i <= 4; i++) {
+			delForm.elements[i].value = table.rows[rowIndex].cells[i+1].innerText;
+		}
 	});
 
-	$('#del,#save').click(function() {
+	$('#save,#del').click(function() {
 		$('#delForm').css({'display':'none'});
+	});
+	$('#del').click(function() {
+		$('#delForm').css({'display':'none'});
+		table.deleteRow(rowIndex);
 	});
 
 /*弹出修改信息*/
 	$('#change').click(function() {
+		$(menu).toggle();
 		$('#changeForm').css({'display':'block'});
+		var changeForm = document.getElementById('changeForm');
+		for (var i = 0; i <= 4; i++) {
+			changeForm.elements[i].value = table.rows[rowIndex].cells[i+1].innerText;
+		}
 	});
 
-	$('#save2, .cancel').click(function() {
+	$('#save2,.cancel').click(function() {
 		$('#changeForm').css({'display':'none'});
+	});
+	$('#save2').click(function() {
+		var changeForm = document.getElementById('changeForm');
+		for (var i = 0; i <= 4; i++) {
+			table.rows[rowIndex].cells[i+1].innerText = changeForm.elements[i].value;
+		}
 	});
 	/*图表*/
 	var myChart = echarts.init(document.getElementById('main'));
 	option1 = {
-		title : {
-			text : '统计数据图',
+		title : {//标题组件，包含主标题和副标题
+			text : '统计图',
+			show : true
 		},
-		tooltip : {
+		tooltip : {//提示框组件
 			trigger: 'item',
 			formatter : function (params) {
 				var date = new Date(params.value[0]);
@@ -72,7 +105,7 @@ $(function() {
 					+ params.value[2];
 			}
 		},
-		toolbox: {
+		toolbox: {//工具栏。内置有导出图片，数据视图，动态类型切换，数据区域缩放，重置五个工具。
 			show : true,
 			feature : {
 				mark : {show: true},
@@ -81,28 +114,29 @@ $(function() {
 				saveAsImage : {show: true}
 			}
 		},
-		dataZoom: {
+		dataZoom: {//用于对数据进行区域缩放，从而能自由关注细节的数据信息，或者概览数据整体
 			show: true,
 			start : 70
 		},
-		legend : {
-			data : ['series1']
+		legend : {//图例组件展现了不同系列的标记(symbol)，颜色和名字。可以通过点击图例控制哪些系列不显示
+			data : ['series1'],
+			z : 1
 		},
-		grid: {
+		grid: {//直角坐标系内绘图网格，单个 grid 内最多可以放置上下两个 X 轴，左右两个 Y 轴。可以在网格上绘制折线图，柱状图，散点图（气泡图）
 			y2: 80
 		},
-		xAxis : [
+		xAxis : [//直角坐标系grid中的x轴，单个 grid 组件最多只能放上下两个 x 轴
 			{
 				type : 'time',
 				splitNumber:10
 			}
 		],
-		yAxis : [
+		yAxis : [//直角坐标系 grid 中的 y 轴，单个 grid 组件最多只能放左右两个 y 轴
 			{
 				type : 'value'
 			}
 		],
-		series : [
+		series : [//系列列表。每个系列通过 type 决定自己的图表类型
 			{
 				name: '1',
 				type: 'line',
@@ -124,12 +158,34 @@ $(function() {
 					}
 					return d;
 				})()
+			},
+			{
+				name: '2',
+				type: 'line',
+				showAllSymbol: true,
+				symbolSize: function (value){
+					return Math.round(value[2]/10) + 2;
+				},
+				data: (function () {
+					var d = [];
+					var len = 0;
+					var now = new Date();
+					var value;
+					while (len++ < 200) {
+						d.push([
+							new Date(2014, 9, 1, 0, len * 3800),
+							(Math.random()*90).toFixed(2) - 0,
+							(Math.random()*100).toFixed(2) - 0
+						]);
+					}
+					return d;
+				})()
 			}
 		]
 	};
-	option2 = {
+	/*option2 = {
 		title : {
-			text : '统计数据图',
+			text : '数据图',
 		},
 		tooltip : {
 			trigger: 'item',
@@ -159,7 +215,8 @@ $(function() {
 			start : 70
 		},
 		legend : {
-			data : ['series1']
+			data : ['series2'],
+			z : 2
 		},
 		grid: {
 			y2: 80
@@ -199,15 +256,10 @@ $(function() {
 				})()
 			}
 		]
-	};
+	};*/
 
 	myChart.setOption(option1);
-	/*var table = $('infoData');
-	table.addEventListener("click", function() {
-		myChart.setOption(option2);
-		myChart.setOption(option1);
-	},false);
-*/
+
 	/*搜索框中的内容*/
 	$(document).ready(function() {
 		$(".search").keyup(function () {
